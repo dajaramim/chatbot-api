1e83338849eb536586f34e4b1c3c4439e2aee734
Daniel Jaramillo <da.jaramim@gmail.com>
2023-06-05 19:26:15 -0400

    endpoints para poblar datos

--- app/resources/agendamiento.py
+++ app/resources/agendamiento.py
@@ -16,8 +16,78 @@ router = APIRouter(
     tags=["agendamiento"],
     responses={404: {"description": "Not found"}},
 )
+##                  POBLAR DATOS
+
+#Ingresar Doctor
+@router.post("/doctor")
+async def create_doctor(data: Doctor, db: AsyncIOMotorClient = Depends(get_db(resource="resource1", method="POST"))):
+    
+    # Convertir el modelo a un diccionario
+    data_dict = data.dict()
+    # Elimino el id que genera pydantic
+    data_dict.pop("id")
+    
+    logging.info(f"post example with: {data_dict}")
+    
+    new_data = await db["doctor"].insert_one(data_dict)
+
+    # Retornar el id del nuevo dato
+    return f"{new_data.inserted_id}"
+
+#Ingresar Paciente
+@router.post("/patient")
+async def create_patient(data: Patient, db: AsyncIOMotorClient = Depends(get_db(resource="resource1", method="POST"))):
+    
+    # Convertir el modelo a un diccionario
+    data_dict = data.dict()
+    # Elimino el id que genera pydantic
+    data_dict.pop("id")
+    
+    logging.info(f"post example with: {data_dict}")
+    
+    new_data = await db["patient"].insert_one(data_dict)
+
+    # Retornar el id del nuevo dato
+    return f"{new_data.inserted_id}"
+
+#Ingresar centro médico
+@router.post("/centre")
+async def create_centre(data: Centre, db: AsyncIOMotorClient = Depends(get_db(resource="resource1", method="POST"))):
+    
+    # Convertir el modelo a un diccionario
+    data_dict = data.dict()
+    # Elimino el id que genera pydantic
+    data_dict.pop("id")
+    
+    logging.info(f"post example with: {data_dict}")
+    
+    new_data = await db["centre"].insert_one(data_dict)
+
+    # Retornar el id del nuevo dato
+    return f"{new_data.inserted_id}"
+
+#Crear una cita
+@router.post("/appoinment")
+async def create_appoinment(data: Appointment, db: AsyncIOMotorClient = Depends(get_db(resource="resource1", method="POST"))):
+    
+    # Convertir el modelo a un diccionario
+    data_dict = data.dict()
+    # Elimino el id que genera pydantic
+    data_dict.pop("id")
+    
+    logging.info(f"post example with: {data_dict}")
+    
+    new_data = await db["appoinment"].insert_one(data_dict)
+
+    # Retornar el id del nuevo dato
+    return f"{new_data.inserted_id}"
+
+
+
+
+# ENDPOINTS QUE NOS PIDE ALLOXENTRIC
 #Obtener todas las campañas
-@router.get("/disponibilidad/{doctor_id}")
+""" @router.get("/disponibilidad/{doctor_id}")
 
 # Obtener una campaña según un id
 @router.post("/agendar")
@@ -33,4 +103,4 @@ router = APIRouter(
 
 @router.put("/sugerencia")
 
-@router.get("/lista_de_espera")
+@router.get("/lista_de_espera") """
--- app/resources/router.py
+++ app/resources/router.py
@@ -1,10 +1,8 @@
 from fastapi import APIRouter
-from app.resources.campaign import router as resources_router_campaign
-from app.resources.category import router as resources_router_category
-from app.resources.subcategory import router as resources_router_subcategory
+from app.resources.agendamiento import router as resources_router_agendamiento
+
 
 
 router = APIRouter()
-router.include_router(resources_router_campaign, prefix="/campaign", tags=["campaign"])
-router.include_router(resources_router_category, prefix="/category", tags=["category"])
-router.include_router(resources_router_subcategory, prefix="/subcategory", tags=["subcategory"])
+router.include_router(resources_router_agendamiento, prefix="/agendamiento", tags=["agendamiento"])
+
