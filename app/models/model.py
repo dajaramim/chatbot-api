336486bc28b982f7abfaf2c51b3a11eca03b46c7
Daniel Jaramillo <da.jaramim@gmail.com>
2023-06-06 20:00:01 -0400

    disponibilidad endpoint

--- app/models/model.py
+++ app/models/model.py
@@ -19,25 +19,56 @@ class PyObjectId(ObjectId):
     def __modify_schema__(cls, field_schema):
         field_schema.update(type="string")
 
+class Patient(BaseModel):
+    id: Optional[PyObjectId] = Field(alias='_id')
+    first_name: str
+    last_name: str
+    rut: str
+    email: str
+    address: str
+    phone: str
+   
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {
+            ObjectId: str
+        }
+
+class Doctor(BaseModel):
+    id: Optional[PyObjectId] = Field(alias='_id')
+    first_name: str
+    last_name: str
+    specialty: str
+    disponibility: List[datetime]
+   
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {
+            ObjectId: str
+        }
+
+class Centre (BaseModel):
+    id: Optional[PyObjectId] = Field(alias='_id')
+    name: str
+    address: str
+   
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {
+            ObjectId: str
+        }
 
 class Appointment (BaseModel):
     id: Optional[PyObjectId] = Field(alias='_id')
-    #paciente
-    patient_name: str
-    patient_rut: str
-    patient_email: str
-    patient_phone: str
-    #doctor
-    doctor_name: str
-    doctor_specialty: str
-    disponibility: datetime
-    #centro médico
-    centre_name: str
-    centre_address: str
-    date: datetime
+    patient: Patient
+    doctor: Doctor
+    centre: Centre
     state: str # si se agendó, canceló, reagendó, ya se hizo la cita
-    updated_at: Optional[datetime]
-    created_at: datetime = datetime.now()
+    updated_date: Optional[datetime]
+    created_date: datetime = datetime.now()
    
     class Config:
         allow_population_by_field_name = True
--- app/resources/agendamiento.py
+++ app/resources/agendamiento.py
@@ -7,6 +7,9 @@ from datetime import datetime
 from motor.motor_asyncio import AsyncIOMotorClient
 from bson import ObjectId
 from app.auth import get_db
+from app.models.model import Patient
+from app.models.model import Doctor
+from app.models.model import Centre
 from app.models.model import Appointment
 
 router = APIRouter(
@@ -15,22 +18,101 @@ router = APIRouter(
 )
 ##                   OBTENER LOS DATOS
 #                    Obtener TODOS LOS DOCTORES
-@router.get("/appoinment")
-async def get_all_doctors(db: AsyncIOMotorClient = Depends(get_db(resource="resource1", method="GET"))) -> List[Appointment]:
+@router.get("/doctor")
+async def get_all_doctors(db: AsyncIOMotorClient = Depends(get_db(resource="resource1", method="GET"))) -> List[Doctor]:
     logging.info("Inside get_all_doctors function")
     try:
         # obtengo todos los elementos de campaign enlistados en un máximo de 100 elementos
-        result = await db["appointment"].find().to_list(length=100)
+        result = await db["doctor"].find().to_list(length=100)
         logging.info(f"Result: {result}")
-        return [Appointment(**appointment) for appointment in result]
+        return [Doctor(**doctor) for doctor in result]
     except Exception as e:
         logging.error(f"Error: {e}")
         raise HTTPException(status_code=500, detail="Internal Server Error")
+    
+
+# Obtener disponibilidad Doctor (lista de fechas disponible)
+@router.get("/doctor/disponibilidad/{doctor_id}")
+async def get_disponibilidad(doctor_id: str, db: AsyncIOMotorClient = Depends(get_db(resource="resource1", method="GET"))):
+    
+    logging.info(f"get example with name: {id}")
+    data = await db["doctor"].find_one({"_id": ObjectId(doctor_id)})
+
+    if data:
+        # Si el dato existe, retornar lista de fechas disponibles
+        return data["disponibility"]
+        # return JSONResponse(status_code=status.HTTP_200_OK, content=ExampleModel(data))
+
+    else:
+        # Si el dato no existe, retorna un error
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, 
+                            content={"message": "Data not found"})
+    
+# Obtener una lista de las citas según el ID del paciente
+
+
+###         SUGERENCIA
+""" @router.get("/sugerencia")
+async def get_sugerencia(db: AsyncIOMotorClient = Depends(get_db(resource="resource1", method="GET"))):
+    
+ """
+
+
 
 ##                    POBLAR DATOS
 
+#Ingresar Doctor
+@router.post("/doctor")
+async def create_doctor(data: Doctor, db: AsyncIOMotorClient = Depends(get_db(resource="resource1", method="POST"))):
+    
+    # Convertir el modelo a un diccionario
+    data_dict = data.dict()
+    # Elimino el id que genera pydantic
+    data_dict.pop("id")
+    
+    logging.info(f"post example with: {data_dict}")
+    
+    new_data = await db["doctor"].insert_one(data_dict)
+
+    # Retornar el id del nuevo dato
+    return f"{new_data.inserted_id}"
+
+#Ingresar Paciente
+@router.post("/patient")
+async def create_patient(data: Patient, db: AsyncIOMotorClient = Depends(get_db(resource="resource1", method="POST"))):
+    
+    # Convertir el modelo a un diccionario
+    data_dict = data.dict()
+    # Elimino el id que genera pydantic
+    data_dict.pop("id")
+    
+    logging.info(f"post example with: {data_dict}")
+    
+    new_data = await db["patient"].insert_one(data_dict)
+
+    # Retornar el id del nuevo dato
+    return f"{new_data.inserted_id}"
+
+
+
+#Ingresar centro médico
+@router.post("/centre")
+async def create_centre(data: Centre, db: AsyncIOMotorClient = Depends(get_db(resource="resource1", method="POST"))):
+    
+    # Convertir el modelo a un diccionario
+    data_dict = data.dict()
+    # Elimino el id que genera pydantic
+    data_dict.pop("id")
+    
+    logging.info(f"post example with: {data_dict}")
+    
+    new_data = await db["centre"].insert_one(data_dict)
+
+    # Retornar el id del nuevo dato
+    return f"{new_data.inserted_id}"
+
 #Crear una cita
-@router.post("/appoinment")
+@router.post("/appointment")
 async def create_appoinment(data: Appointment, db: AsyncIOMotorClient = Depends(get_db(resource="resource1", method="POST"))):
     
     # Convertir el modelo a un diccionario
@@ -40,7 +122,7 @@ async def create_appoinment(data: Appointment, db: AsyncIOMotorClient = Depends(
     
     logging.info(f"post example with: {data_dict}")
     
-    new_data = await db["appoinment"].insert_one(data_dict)
+    new_data = await db["appointment"].insert_one(data_dict)
 
     # Retornar el id del nuevo dato
     return f"{new_data.inserted_id}"
@@ -51,7 +133,7 @@ async def create_appoinment(data: Appointment, db: AsyncIOMotorClient = Depends(
 
 """ 
 Daniel
-@router.get("/disponibilidad/{doctor_id}")
+@router.get("/disponibilidad/{doctor_id}") LISTO
 @router.get("/sugerencia")
 @router.get("/lista_de_espera") 
 
@@ -65,17 +147,26 @@ Ricardo
 
 """
 
+
+
 # confirmar una cita
-# 1- get paciente
-# 2- get appointment según id paciente
-# 3- get doctor(muestra nombre y especialidad)
-# 4- get centro médico con el id del doctor
-# 5- put appointment (fecha)
+#  
+# 1- get appointment según id paciente
+# 2- get doctor(muestra nombre y especialidad)
+# 3- get centro médico con el id del doctor
+# 4- put appointment (fecha y estado)
+
+# Confirmar multiples Citas
+# 1- get appointment según id paciente
+# 2- Muestra las citas una a una (CHATBOT)
+ 
+
+# Cancelación espontánea
+# 1- get appointment según id paciente
 # 
-# 
-# 
-# 
-# 
-# 
-# ##
-# #
+#
+#   
+
+# Reagendamiento automático
+# 1- get appointment según id paciente
+# 2- /disponibilidad
